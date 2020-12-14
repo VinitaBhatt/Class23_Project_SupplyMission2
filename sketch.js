1ec15a90631129cfc183cf94e6b82032020a31c9
VinitaBhatt <71602249+VinitaBhatt@users.noreply.github.com>
2020-12-14 13:28:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var box1, box2, box3;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+
+	 box1 = new Box(width/2 - 110, 600,20,100);
+	 box2 = new Box(width/2, 640,200,20);
+	 box3 = new Box(width/2 + 110 , 600,20,100);
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
+
+  box1.display();
+  box2.display();
+  box3.display();
+
+  fill("white");
+  rect(ground.position.x,ground.position.y,width,10);
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+
+	if (keyCode === LEFT_ARROW) {
+
+		helicopterSprite.x=helicopterSprite.x-20;    
+		translation={x:-20,y:0}
+		Matter.Body.translate(packageBody, translation)
+	
+	} else if (keyCode === RIGHT_ARROW) {
+
+		helicopterSprite.x=helicopterSprite.x+20;
+		translation={x:20,y:0}
+		Matter.Body.translate(packageBody, translation)
+
+	  }else if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(packageBody,false);
+ 	}
+}
+
+
+
